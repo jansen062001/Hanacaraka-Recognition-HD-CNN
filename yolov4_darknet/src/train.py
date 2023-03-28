0c382a34bbc4e22cb0d2f6bb8c3c8c4239408564
jansen062001 <jansen062001@gmail.com>
2023-03-28 21:14:10 +0700

    refactor: add config file

--- yolov4_darknet/src/train.py
+++ yolov4_darknet/src/train.py
@@ -3,27 +3,21 @@ import glob
 import shutil
 import random
 
-YOLO_WEIGHTS_URL = 'https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.conv.137'
-YOLO_WEIGHTS_NAME = 'yolov4.conv.137'
-NUMBER_OF_CLASSES = 9
-PERCENTAGE_TRAIN = 90
-PERCENTAGE_TEST = 100 - PERCENTAGE_TRAIN
-IMG_DATASET_EXT = ['jpg', 'png', 'jpeg']
-YOLO_CFG_FILENAME = 'yolov4-custom.cfg'
+from config import *
 
 
 def make_train_dir():
-    if os.path.exists(os.getcwd() + '/training') == False:
-        os.mkdir(os.getcwd() + '/training')
+    if os.path.exists(TRAIN_RESULT_DIR) == False:
+        os.mkdir(TRAIN_RESULT_DIR)
 
 
 def build_darknet():
-    current_dir = os.getcwd()
+    os.chdir(WORKING_DIR)
 
     cmd = 'git clone https://github.com/AlexeyAB/darknet.git'
     os.system(cmd)
 
-    os.chdir(os.path.join(current_dir, 'darknet'))
+    os.chdir(DARKNET_DIR)
 
     cmd = 'sed -i "s/OPENCV=0/OPENCV=1/" Makefile;' + \
         'sed -i "s/GPU=0/GPU=1/" Makefile;' + \
@@ -36,55 +30,45 @@ def build_darknet():
     cmd = 'wget ' + YOLO_WEIGHTS_URL
     os.system(cmd)
 
-    os.chdir(current_dir)
-
 
 def move_dataset_folder():
-    current_dir = os.getcwd()
-    dataset_dir = os.path.join(current_dir, 'dataset')
-    target_dir = os.path.join(current_dir, 'darknet', 'data', 'obj')
-
-    if os.path.exists(target_dir) == False:
-        os.mkdir(target_dir)
+    if os.path.exists(DARKNET_DATA_DIR) == False:
+        os.mkdir(DARKNET_DATA_DIR)
 
     for ext_name in IMG_DATASET_EXT:
-        for path_and_filename in glob.iglob(dataset_dir + '/' + '*.' + ext_name):
+        for path_and_filename in glob.iglob(os.path.join(DATA_DIR, '*.' + ext_name)):
             file_title, file_ext = os.path.splitext(
                 os.path.basename(path_and_filename))
 
             shutil.move(path_and_filename,
-                        target_dir + '/' + file_title + file_ext)
+                        os.path.join(DARKNET_DATA_DIR, file_title + file_ext))
 
-            shutil.move(dataset_dir + '/' + file_title + '.txt',
-                        target_dir + '/' + file_title + '.txt')
+            shutil.move(os.path.join(DATA_DIR, file_title + '.txt'),
+                        os.path.join(DARKNET_DATA_DIR, file_title + '.txt'))
 
 
 def create_obj_data():
-    with open(os.path.join(os.getcwd(), 'darknet', 'data', 'obj.data'), 'w') as f:
+    with open(os.path.join(DARKNET_DIR, 'data', 'obj.data'), 'w') as f:
         f.write('classes = ' + str(NUMBER_OF_CLASSES) + '\n')
         f.write('train = data/train.txt' + '\n')
         f.write('valid = data/test.txt' + '\n')
         f.write('names = data/obj.names' + '\n')
-        f.write('backup = ../training')
+        f.write('backup = ../' + TRAIN_RESULT_DIRNAME)
 
 
 def create_obj_names():
-    dataset_dir = os.path.join(os.getcwd(), 'dataset')
-    data_dir = os.path.join(os.getcwd(), 'darknet', 'data')
-
-    shutil.move(dataset_dir + '/classes.txt',
-                data_dir + '/obj.names')
+    shutil.move(os.path.join(DATA_DIR, 'classes.txt'),
+                os.path.join(DARKNET_DIR, 'data', 'obj.names'))
 
 
 def split_train_valid():
-    data_dir = os.path.join(os.getcwd(), 'darknet', 'data')
-    file_train = open(os.path.join(data_dir, 'train.txt'), 'w')
-    file_test = open(os.path.join(data_dir, 'test.txt'), 'w')
+    file_train = open(os.path.join(DARKNET_DIR, 'data', 'train.txt'), 'w')
+    file_test = open(os.path.join(DARKNET_DIR, 'data', 'test.txt'), 'w')
     dataset = []
     test_data_idx = []
 
     for ext_name in IMG_DATASET_EXT:
-        for path_and_filename in glob.iglob(data_dir + '/obj/' + '*.' + ext_name):
+        for path_and_filename in glob.iglob(os.path.join(DARKNET_DATA_DIR, '*.' + ext_name)):
             dataset.append(path_and_filename)
 
     while True:
@@ -110,30 +94,19 @@ def split_train_valid():
 
 
 def move_cfg_file():
-    cfg_dir = os.path.join(os.getcwd(), 'darknet', 'cfg')
-
-    shutil.move(os.path.join(os.getcwd(), YOLO_CFG_FILENAME),
-                cfg_dir + '/' + YOLO_CFG_FILENAME)
+    shutil.move(os.path.join(WORKING_DIR, YOLO_CFG_FILENAME),
+                os.path.join(DARKNET_DIR, 'cfg', YOLO_CFG_FILENAME))
 
 
 def run_training():
-    current_dir = os.getcwd()
-
-    os.chdir(os.path.join(current_dir, 'darknet'))
+    os.chdir(DARKNET_DIR)
 
     cmd = './darknet detector train data/obj.data cfg/' + \
         YOLO_CFG_FILENAME + ' ' + YOLO_WEIGHTS_NAME + ' -dont_show -map'
     os.system(cmd)
 
-    os.chdir(current_dir)
-
 
 if __name__ == "__main__":
-    # Change working directory
-    working_dir = os.path.abspath(
-        os.path.dirname(os.path.abspath(__file__)) + '/../')
-    os.chdir(working_dir)
-
     make_train_dir()
     build_darknet()
     move_dataset_folder()
