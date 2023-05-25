63ac0823c1f610d63d58a3fd781267a8f8dfc0f0
jansen062001 <jansen062001@gmail.com>
2023-05-25 20:57:26 +0700

    refactor: wrap run yolo test as function

--- yolov4_darknet/src/test.py
+++ yolov4_darknet/src/test.py
@@ -7,7 +7,7 @@ import cv2
 from .config import *
 
 
-def create_cfg_test_file(cfg_filename):
+def create_cfg_test_file(cfg_filename, width, height):
     os.chdir(os.path.join(DARKNET_DIR, "cfg"))
 
     shutil.copyfile(YOLO_CFG_FILENAME, cfg_filename)
@@ -16,16 +16,16 @@ def create_cfg_test_file(cfg_filename):
         'sed -i "s/batch=64/batch=1/" '
         + cfg_filename
         + ";"
-        + 'sed -i "s/subdivisions=64/subdivisions=1/" '
+        + 'sed -i "s/subdivisions=2/subdivisions=1/" '
         + cfg_filename
         + ";"
         + 'sed -i "s/width=416/width='
-        + args.width
+        + str(width)
         + '/" '
         + cfg_filename
         + ";"
         + 'sed -i "s/height=416/height='
-        + args.height
+        + str(height)
         + '/" '
         + cfg_filename
         + ";"
@@ -33,7 +33,7 @@ def create_cfg_test_file(cfg_filename):
     os.system(cmd)
 
 
-def run_test(cfg_filename, test_img_filename):
+def detect(cfg_filename, test_img_filename):
     os.chdir(os.path.join(WORKING_DIR, "test_images"))
 
     img = cv2.imread(test_img_filename, 0)
@@ -66,6 +66,14 @@ def remove_cfg_test_file(cfg_filename):
     os.remove(cfg_filename)
 
 
+def run_test(width, height, filename):
+    cfg_test_filename = uuid.uuid4().hex + ".cfg"
+
+    create_cfg_test_file(cfg_test_filename, width, height)
+    detect(cfg_test_filename, filename)
+    remove_cfg_test_file(cfg_test_filename)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--width", type=str, required=True)
@@ -73,8 +81,4 @@ if __name__ == "__main__":
     parser.add_argument("--filename", type=str, required=True)
     args = parser.parse_args()
 
-    cfg_test_filename = uuid.uuid4().hex + ".cfg"
-
-    create_cfg_test_file(cfg_test_filename)
-    run_test(cfg_test_filename, args.filename)
-    remove_cfg_test_file(cfg_test_filename)
+    run_test(args.width, args.height, args.filename)
