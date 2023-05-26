1df8ac7f514dada9559546e83e9ae3b30bc3462a
jansen062001 <jansen062001@gmail.com>
2023-05-26 20:31:57 +0700

    refactor: add option to change 'working dir' to initial

--- yolov4_darknet/src/test.py
+++ yolov4_darknet/src/test.py
@@ -8,70 +8,79 @@ from .config import *
 
 
 def create_cfg_test_file(cfg_filename, width, height):
-    os.chdir(os.path.join(DARKNET_DIR, "cfg"))
+    darknet_cfg_dir = os.path.join(DARKNET_DIR, "cfg")
+    test_cfg_path = os.path.join(darknet_cfg_dir, cfg_filename)
 
-    shutil.copyfile(YOLO_CFG_FILENAME, cfg_filename)
+    shutil.copyfile(
+        os.path.join(darknet_cfg_dir, YOLO_CFG_FILENAME),
+        test_cfg_path,
+    )
 
     cmd = (
         'sed -i "s/batch=64/batch=1/" '
-        + cfg_filename
+        + test_cfg_path
         + ";"
         + 'sed -i "s/subdivisions=2/subdivisions=1/" '
-        + cfg_filename
+        + test_cfg_path
         + ";"
         + 'sed -i "s/width=416/width='
         + str(width)
         + '/" '
-        + cfg_filename
+        + test_cfg_path
         + ";"
         + 'sed -i "s/height=416/height='
         + str(height)
         + '/" '
-        + cfg_filename
+        + test_cfg_path
         + ";"
     )
     os.system(cmd)
 
 
 def detect(cfg_filename, test_img_filename):
-    os.chdir(os.path.join(WORKING_DIR, "test_images"))
+    test_img_dir = os.path.join(WORKING_DIR, "test_images")
 
-    img = cv2.imread(test_img_filename, 0)
+    img = cv2.imread(os.path.join(test_img_dir, test_img_filename), 0)
     filename = uuid.uuid4().hex + ".jpg"
-    cv2.imwrite(filename, img)
+    cv2.imwrite(os.path.join(test_img_dir, filename), img)
 
     os.chdir(DARKNET_DIR)
-
-    cmd = (
-        "./darknet detector test data/obj.data cfg/"
-        + cfg_filename
-        + " ../"
-        + TRAIN_RESULT_DIRNAME
-        + "/yolov4-tiny_final.weights -ext_output -dont_show ../test_images/"
-        + filename
-        + " > ../test_images/result.txt"
+    cmd = "{} detector test {} {} {} -ext_output -dont_show {} > {}"
+    os.system(
+        cmd.format(
+            os.path.join(DARKNET_DIR, "darknet"),
+            os.path.join(DARKNET_DIR, "data", "obj.data"),
+            os.path.join(DARKNET_DIR, "cfg", cfg_filename),
+            os.path.join(TRAIN_RESULT_DIR, "yolov4-tiny_final.weights"),
+            os.path.join(test_img_dir, filename),
+            os.path.join(test_img_dir, "result.txt"),
+        )
     )
-    os.system(cmd)
 
-    cmd = "cp predictions.jpg ../test_images/output_" + test_img_filename
-    os.system(cmd)
+    cmd = "cp {} {}"
+    os.system(
+        cmd.format(
+            os.path.join(DARKNET_DIR, "predictions.jpg"),
+            os.path.join(test_img_dir, "output_" + test_img_filename),
+        )
+    )
 
-    os.chdir(os.path.join(WORKING_DIR, "test_images"))
-    os.remove(filename)
+    os.remove(os.path.join(WORKING_DIR, "test_images", filename))
 
 
 def remove_cfg_test_file(cfg_filename):
-    os.chdir(os.path.join(DARKNET_DIR, "cfg"))
-
-    os.remove(cfg_filename)
+    os.remove(os.path.join(DARKNET_DIR, "cfg", cfg_filename))
 
 
-def run_test(width, height, filename):
+def run_test(width, height, filename, change_working_dir=False, working_dir_path=None):
     cfg_test_filename = uuid.uuid4().hex + ".cfg"
 
     create_cfg_test_file(cfg_test_filename, width, height)
     detect(cfg_test_filename, filename)
     remove_cfg_test_file(cfg_test_filename)
+
+    if change_working_dir:
+        os.chdir(working_dir_path)
 
 
 if __name__ == "__main__":
